468df4dca1dcb23392dc3cabd36308a57bf49cdc
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-02-23 13:45:27 +0100

    Minor change to dameon reload script

--- sh/daemon_reload.sh
+++ sh/daemon_reload.sh
@@ -8,8 +8,6 @@ main()
 {
 	source $REWRITER_WS_HOME/sh/daemon_stop.sh
 	source $REWRITER_WS_HOME/sh/daemon_start.sh
-	sleep 3.0
-	source $REWRITER_WS_HOME/sh/daemon_status.sh
 }
 
 # Invoke entry point.
--- sh/daemon_start.sh
+++ sh/daemon_start.sh
@@ -8,8 +8,10 @@ main()
 {
 	source $REWRITER_WS_HOME/sh/reset_logs.sh
 	supervisord -c $REWRITER_WS_HOME/ops/config/supervisord.conf
-
 	log "initialized web-service daemon"
+
+	sleep 3.0
+	source $REWRITER_WS_HOME/sh/daemon_status.sh
 }
 
 # Invoke entry point.
