03a00348d375b7b6d2d99426dcea155478de3c9f
Mark A. Greenslade <momipsl@ipsl.jussieu.fr>
2017-12-15 20:47:40 +0100

    Improved cmip6 further url resolution

--- url_rewriter/factory/further_info.py
+++ url_rewriter/factory/further_info.py
@@ -9,7 +9,7 @@
 
 
 """
-import pyessv
+from pyessv import vocabs
 
 
 
@@ -28,16 +28,20 @@ def _yield_cmip6():
 	NOTE 2: Normative form: mip_era.institution_id.source_id.experiment_id.sub_experiment_id.variant_label
 
 	"""
-	for source in pyessv.load('wcrp:cmip6:source-id'):
-		for institution_id in source.data['institution_id']:
-			institution = pyessv.load('wcrp:cmip6:institution-id:{}'.format(institution_id))
-			for experiment in pyessv.load('wcrp:cmip6:experiment-id'):
-				for sub_experiment_id in [i for i in experiment.data['sub_experiment_id']]:
+	# Set pointer to CMIP6 vocabs accessor.
+	cmip6 = vocabs.wcrp.cmip6
+
+	# Yield URL's based upon vocab canonical or raw names.
+	for source in cmip6.source_id:
+		for institution in [cmip6.institution_id[i] for i in source.institution_id]:
+			for experiment in cmip6.experiment_id:
+				for sub_experiment_id in experiment.sub_experiment_id:
 					for field in {'canonical_name', 'raw_name'}:
-						yield r'/{}.{}.{}.{}.{}.r[0-9]i[0-9]p[0-9]f[0-9]'.format(
+						yield r'/{}.{}.{}.{}.{}.{}'.format(
 							'cmip6' if field == 'canonical_name' else 'CMIP6',
 							getattr(institution, field),
 							getattr(source, field),
 							getattr(experiment, field),
-							sub_experiment_id
+							sub_experiment_id,
+							cmip6.ensemble.term_regex
 							)
